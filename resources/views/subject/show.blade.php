6c2ea3fd874aa9af85e3ce56d86adb8d6972102e
KrisCatDog <ramain77@gmail.com>
2019-10-12 21:44:43 +0700

    remove comment

--- resources/views/subject/show.blade.php
+++ resources/views/subject/show.blade.php
@@ -8,12 +8,7 @@
 
     <div class="card-body">
         <h3 class="card-title">{{ $subject->name }}</h3>
-        {{-- <ul class="flex-grow-1 mt-4">
-            <h5 class="card-subtitle mb-2 text-muted">Classes with This Subject</h5>
-            @foreach ($subject->classes as $class)
-            <li class="card-text">{{ $class->name }}</li>
-        @endforeach
-        </ul> --}}
+
         <a href="{{ route('subjects.index') }}" class="card-link btn btn-outline-success">Back</a>
     </div>
 
